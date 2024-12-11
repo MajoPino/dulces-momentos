b5cd9cc82b3bd97b4a5ead35719952ca1af4993b
MajoPino <majo12pino@gmail.com>
2024-12-11 09:19:13 -0500

    feat: Add new fields to the create users table migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -16,6 +16,9 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('address');
+            $table->string('phone_number');
+            $table->integer('role');
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
