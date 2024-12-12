133763a72ad25e025ca2bad6f111e11923afb132
MajoPino <majo12pino@gmail.com>
2024-12-12 07:47:31 -0500

    feat: Create the orders table migration

---
+++ database/migrations/2024_12_12_123306_create_orders_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->integer('product_quantity');
+            $table->decimal('total_price');
+            $table->boolean('order_status');
+            $table->dateTime('shipment_date');
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('product_id')->constrained('products')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
